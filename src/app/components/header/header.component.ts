f4df210a5e79b06d01d31fa34bba43c13edc5ddf
Natalia Ortiz <natalia.ortiz@192.168.1.4>
2025-10-01 12:26:17 -0500

    estilos globales

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,27 +1,18 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterModule, Router } from "@angular/router";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
   menuItems = [
-    { label: "Inicio", path: "/", active: true },
-    { label: "Conoce Kash", path: "/conoce-kash", active: false },
-    { label: "Resultados", path: "/resultados", active: false },
-    { label: "Premios", path: "/premios", active: false },
+    { label: "Inicio", active: true },
+    { label: "Conoce Kash", active: false },
+    { label: "Resultados", active: false },
+    { label: "Premios", active: false },
   ];
-
-  constructor(private router: Router) {
-    this.router.events.subscribe(() => {
-      this.menuItems.forEach(item => {
-        item.active = this.router.url === item.path;
-      });
-    });
-  }
 }
