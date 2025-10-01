c1bf8bdf26fc37a1f09c8613667741bd78d53813
Natalia Ortiz <natalia.ortiz@192.168.1.4>
2025-10-01 13:07:11 -0500

    rutas completas header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
 import { ConoceKashComponent } from "./pages/conoce-kash/conoce-kash.component";
+import { ResultadosComponent } from './pages/resultados/resultados.component';
+import { PremiosComponent } from './pages/premios/premios.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
-    { path: "conoce-kash", component: ConoceKashComponent }
+    { path: "conoce-kash", component: ConoceKashComponent },
+    { path: "resultados", component: ResultadosComponent },
+    { path: "premios", component: PremiosComponent }
 ];
---
+++ src/app/pages/premios/premios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-premios',
+  standalone: true,
+  imports: [],
+  templateUrl: './premios.component.html',
+  styleUrl: './premios.component.scss'
+})
+export class PremiosComponent {
+
+}
